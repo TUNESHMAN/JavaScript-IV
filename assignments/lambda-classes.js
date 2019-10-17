4a50e66337c2987a81f2097ed80c5d31ac974385
Babatunde <babatundea15@gmail.com>
2019-10-17 23:17:45 +0100

    i added speak method to the person constructor

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -1,10 +1,11 @@
 // CODE here for your Lambda Classes
 class Person {
-    constructor (name, age, location){
+  constructor(name, age, location) {
     this.name = name;
     this.age = age;
     this.location = location;
-
-
-    }
+  }
+  speak () {
+      return `Hello my name is ${this.name}, I am from ${this.location}`
+  }
 }
