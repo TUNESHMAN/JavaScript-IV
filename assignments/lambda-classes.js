54b703b83016c1fabba84c6aec77150b1a64b6b1
Babatunde <babatundea15@gmail.com>
2019-10-17 23:25:21 +0100

    I created 2 instances of Person

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -9,3 +9,8 @@ class Person {
       return `Hello my name is ${this.name}, I am from ${this.location}`
   }
 }
+// The 2 Person's are Joel and Tasha
+const Person1 = new Person (`Joel`, 30 , `Pretoria`);
+const Person2 = new Person (`Tasha`, 25 , `Manchester`);
+console.log(Person1, Person2);
+
