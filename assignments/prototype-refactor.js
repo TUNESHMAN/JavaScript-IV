82c9011417f6d8a3a812cc042d93b39578866227
Babatunde <babatundea15@gmail.com>
2019-10-17 21:51:43 +0100

    prototype refractor done

--- assignments/prototype-refactor.js
+++ assignments/prototype-refactor.js
@@ -18,23 +18,23 @@ Prototype Refactor
 
 // EXAMPLE SOLUTION CODE:
 function Airplane(name) {
-    this.name = name;
-    this.isFlying = false;
-  }
-  Airplane.prototype.takeOff = function() {
-    this.isFlying = true;
-  };
-  Airplane.prototype.land = function() {
-    this.isFlying = false;
-  };
-  
-  /*
+  this.name = name;
+  this.isFlying = false;
+}
+Airplane.prototype.takeOff = function() {
+  this.isFlying = true;
+};
+Airplane.prototype.land = function() {
+  this.isFlying = false;
+};
+
+/*
   // 👇 COMPLETE YOUR WORK BELOW 👇
   // 👇 COMPLETE YOUR WORK BELOW 👇
   // 👇 COMPLETE YOUR WORK BELOW 👇
   */
-  
-  /*
+
+/*
     TASK 1
       - Write a Person Constructor that initializes `name` and `age` from arguments.
       - All instances of Person should initialize with an empty `stomach` array.
@@ -46,29 +46,30 @@ function Airplane(name) {
       - Give instances of Person a method `.toString()`:
           + It should return a string with `name` and `age`. Example: "Mary, 50"
   */
-  
-  function Person(name, age) {
+
+class Person {
+  constructor(name, age) {
     this.name = name;
     this.age = age;
     this.stomach = [];
   }
-  Person.prototype.eat = function(edible) {
+  eat() {
     if (this.stomach.length > 9) {
       return null;
     } else {
       this.stomach.push(edible);
     }
-  };
-  
-  Person.prototype.poop = function() {
+  }
+  poop() {
     return (this.stomach = []);
-  };
-  
-  Person.prototype.toString = function() {
+  }
+  toString() {
     return `${this.name}, ${this.age}`;
-  };
-  
-  /*
+  }
+}
+console.log(Person.eat);
+
+/*
     TASK 2
       - Write a Car constructor that initializes `model` and `milesPerGallon` from arguments.
       - All instances built with Car:
@@ -81,47 +82,52 @@ function Airplane(name) {
       - A car which runs out of `fuel` while driving can't drive any more distance:
           + The `drive` method should return a string "I ran out of fuel at x miles!" x being `odometer`.
   */
-  
-  function Car(model, milesPerGallon) {
+
+class Car {
+  constructor(model, milesPerGallon) {
     this.model = model;
     this.milesPerGallon = milesPerGallon;
     this.tank = 0;
     this.odometer = 0;
   }
-  Car.prototype.fill = function(gallons) {
+  fill(gallons) {
     this.tank += gallons;
-  };
-  
-  Car.prototype.drive = function(distance) {
-    if (this.tank < distance / this.milesPerGallon) {
-     this.odometer += this.milesPerGallon * this.tank;
-     this.tank =0;
-     return `I ran out of fuel at ${this.odometer} miles`
   }
-  };
-  
-  /*
+
+  drive(distance) {
+    if (this.tank < distance / this.milesPerGallon) {
+      this.odometer += this.milesPerGallon * this.tank;
+      this.tank = 0;
+      return `I ran out of fuel at ${this.odometer} miles`;
+    }
+  }
+}
+
+/*
     TASK 3
       - Write a Baby constructor subclassing Person.
       - Besides `name`, Baby takes a second argument to initialize `favoriteToy`.
       - Besides the methods on Person.prototype, babies have the ability to `.play()`:
           + Should return a string "Playing with x", x being the favorite toy.
   */
+
+class Baby extends Person {
+    constructor (name, favoriteToy){
+        super(name, favoriteToy); 
+        this.favoriteToy = favoriteToy;
+
+    }
+    play() {
+        return ` Playing with ${this.favoriteToy}`;
+      };
   
-  function Baby(name,favoriteToy) {
-  
-    Person.call(this,name);
-    this.favoriteToy = favoriteToy;
-  }
-  
-  Baby.prototype = Object.create(Person.prototype);
-  
-  Baby.prototype.play = function (){
-    return ` Playing with ${this.favoriteToy}`
-  
-  }
-  
-  /* 
+}
+
+
+
+
+
+/* 
     TASK 4
   
     In your own words explain the four principles for the "this" keyword below:
@@ -130,23 +136,22 @@ function Airplane(name) {
     3. Explicit binding: In explicit binding, the "this" keyword is used to explicitly set the context of the object. Here, we make use of call, apply, and bind.
     4. The new keyword: With the new keyword, "this" refers to an object created with the constructor function.
   */
-  
-  ///////// END OF CHALLENGE /////////
-  ///////// END OF CHALLENGE /////////
-  ///////// END OF CHALLENGE /////////
-  if (typeof exports !== "undefined") {
-    module.exports = module.exports || {};
-    if (Airplane) {
-      module.exports.Airplane = Airplane;
-    }
-    if (Person) {
-      module.exports.Person = Person;
-    }
-    if (Car) {
-      module.exports.Car = Car;
-    }
-    if (Baby) {
-      module.exports.Baby = Baby;
-    }
+
+///////// END OF CHALLENGE /////////
+///////// END OF CHALLENGE /////////
+///////// END OF CHALLENGE /////////
+if (typeof exports !== "undefined") {
+  module.exports = module.exports || {};
+  if (Airplane) {
+    module.exports.Airplane = Airplane;
   }
-  
+  if (Person) {
+    module.exports.Person = Person;
+  }
+  if (Car) {
+    module.exports.Car = Car;
+  }
+  if (Baby) {
+    module.exports.Baby = Baby;
+  }
+}
