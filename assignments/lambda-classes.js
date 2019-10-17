8f44f989c89e2998a395c3336b900b039e2401de
Babatunde <babatundea15@gmail.com>
2019-10-18 00:02:22 +0100

    i setup class for instructor

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -5,12 +5,34 @@ class Person {
     this.age = age;
     this.location = location;
   }
-  speak () {
-      return `Hello my name is ${this.name}, I am from ${this.location}`
+  speak() {
+    return `Hello my name is ${this.name}, I am from ${this.location}`;
   }
 }
 // The 2 Person's are Joel and Tasha
-const Person1 = new Person (`Joel`, 30 , `Pretoria`);
-const Person2 = new Person (`Tasha`, 25 , `Manchester`);
+const Person1 = new Person(`Joel`, 30, `Pretoria`);
+const Person2 = new Person(`Tasha`, 25, `Manchester`);
 console.log(Person1, Person2);
+
+// This is the instructor class
+class Instructor extends Person {
+  constructor(name, age, location, specialty, favLang, catchPhrase) {
+    super(name, age, location);
+    this.specialty = specialty;
+    this.favLang = favLang;
+    this.catchPhrase = catchPhrase;
+  }
+
+  demo(subject) {
+    return `Today we are learning about ${subject}`;
+  }
+  grade (student){
+      return `${student.name} receives a perfect score on ${subject}`;
+
+  }
+}
+const Instructor1 = new Instructor (`Gabriel`, 42 , `Madrid`, `Redux`, `Python`, `Don't forget the homies` )
+const Instructor2 = new Instructor (`Alex`, 28,`California`, `Android`, `React`, `I love you all` )
+console.log(Instructor1.demo());
+console.log(Instructor2.grade());
 
